861be3dd1547c737698fdc792ff48992cf39a4ef
9dl <80607793+9dl@users.noreply.github.com>
2022-10-12 02:07:39 +0200

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecurityRequests
+{
+    public class Req
+    {
+        public static string SendGet(string Url)
+        {
+            var Response = "";
+            try
+            {
+                string JunkStr = "https://auth.gg/newreq";
+                string JunkStr2 = "https://keyauth.win/api";
+                string JunkStr3 = "https://gtihub.com/" + Environment.UserName + "/Backend";
+
+                var Request = new Leaf.xNet.HttpRequest();
+                Request.SslCertificateValidatorCallback = (RemoteCertificateValidationCallback)Delegate.Combine(Request.SslCertificateValidatorCallback, new RemoteCertificateValidationCallback((object obj, X509Certificate cert, X509Chain ssl, SslPolicyErrors error) => (cert as X509Certificate2).Verify()));
+                Response = Request.Get(Url).ToString();
+            }
+            catch { }
+
+            return Response;
+        }
+    }
+}
